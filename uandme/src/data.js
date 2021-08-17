90bb7b58e3e9609783e11879cb8048c71fba103b
Hibjul Ahmed <hibjulahmed@gmail.com>
2021-08-17 22:59:54 +0530

    Product Screen Added

--- uandme/src/data.js
+++ uandme/src/data.js
@@ -4,72 +4,78 @@ const data= {
         name:'Dell Insperion',
         category:'Laptop',
         image:'/images/product1.jpg',
-        currency:'$',
+        currency:'₹',
         price:120,
         brand:'Dell',
         rating:4.5,
         numReviews:10,
-        description:"High Quality Products"
+        description:"High Quality Products",
+        countInStock:10,
     },
     {
         _id:'2',
         name:'HP Pabillion 15',
         category:'Laptop',
         image:'/images/product2.jpg',
-        currency:'$',
+        currency:'₹',
         price:140,
         brand:'HP',
         rating:4.0,
         numReviews:10,
-        description:"High Quality Products"
+        description:"High Quality Products",
+        countInStock:10
     },
     {
         _id:'3',
         name:'Asus VivoBook 15',
         category:'Laptop',
         image:'/images/product3.jpg',
-        currency:'$',
+        currency:'₹',
         price:110,
         brand:'Asus',
         rating:3.5,
         numReviews:10,
-        description:"High Quality Products"
+        description:"High Quality Products",
+        countInStock:10
     },
     {
         _id:'4',
         name:'Dell Insperion',
         category:'Laptop',
         image:'/images/product1.jpg',
-        currency:'$',
+        currency:'₹',
         price:120,
         brand:'Dell',
         rating:4.5,
         numReviews:10,
-        description:"High Quality Products"
+        description:"High Quality Products",
+        countInStock:0
     },
     {
         _id:'5',
         name:'Dell Insperion',
         category:'Laptop',
         image:'/images/product1.jpg',
-        currency:'$',
+        currency:'₹',
         price:120,
         brand:'Dell',
         rating:4.5,
         numReviews:10,
-        description:"High Quality Products"
+        description:"High Quality Products",
+        countInStock:10
     },
     {
         _id:'6',
         name:'Dell Insperion',
         category:'Laptop',
         image:'/images/product1.jpg',
-        currency:'$',
+        currency:'₹',
         price:120,
         brand:'Dell',
         rating:4.5,
         numReviews:10,
-        description:"High Quality Products"
+        description:"High Quality Products",
+        countInStock:10
     }]
     
 }
